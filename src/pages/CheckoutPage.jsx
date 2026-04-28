456104c42da93c389c0bcfa07b8574750972bca6
Eyohen <henry.eyo2@gmail.com>
2026-04-28 15:38:24 +0100

    changed to henry

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -44,8 +44,8 @@ function CheckoutPage() {
 
     // Merchant public key for API authentication
     // const MERCHANT_PUBLIC_KEY = 'pk_95eb0b7a2fc51bc343527ecc61a90617';
-    // const MERCHANT_PUBLIC_KEY = 'pk_55b618684a25669f942bd4b21a4d61c4';
-    const MERCHANT_PUBLIC_KEY = 'pk_95eb0b7a2fc51bc343527ecc61a90617';
+    const MERCHANT_PUBLIC_KEY = 'pk_55b618684a25669f942bd4b21a4d61c4';
+    // const MERCHANT_PUBLIC_KEY = 'pk_95eb0b7a2fc51bc343527ecc61a90617';
 
     const API_URL = 'https://talented-mercy-production.up.railway.app';
 
